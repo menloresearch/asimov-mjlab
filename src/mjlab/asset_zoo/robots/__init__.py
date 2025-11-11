0218d05cf9d777889fa14ceb5da6a45173c00fe3
Ishneet0710 <ishneetsingh0710@gmail.com>
2025-11-11 12:54:22 +0800

    working asimov walking policy

--- src/mjlab/asset_zoo/robots/__init__.py
+++ src/mjlab/asset_zoo/robots/__init__.py
@@ -1,3 +1,6 @@
+from mjlab.asset_zoo.robots.asimov.asimov_constants import (
+  get_asimov_robot_cfg as get_asimov_robot_cfg,
+)
 from mjlab.asset_zoo.robots.unitree_g1.g1_constants import (
   get_g1_robot_cfg as get_g1_robot_cfg,
 )
@@ -6,6 +9,7 @@ from mjlab.asset_zoo.robots.unitree_go1.go1_constants import (
 )
 
 __all__ = (
+  "get_asimov_robot_cfg",
   "get_g1_robot_cfg",
   "get_go1_robot_cfg",
 )
